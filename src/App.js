fbca3b3319feb7c0caa04f0156b63232cbbe4e3a
Deepansh Singh Arya <deepansharya1111@gmail.com>
2022-04-27 22:12:42 +0530

    Fix App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ let stream = require( './ws/stream' );
 let path = require( 'path' );
 let favicon = require( 'serve-favicon' );
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+
 
 app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
 app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
@@ -19,4 +17,4 @@ app.get( '/', ( req, res ) => {
 
 io.of( '/stream' ).on( 'connection', stream );
 
-
+server.listen( process.env.PORT || 3000 );
